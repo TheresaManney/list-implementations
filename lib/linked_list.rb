c2a34d78daadbf6faeda3a9af824a23a13481e9e
Theresa Manney <theresa.m.17@gmail.com>
2017-04-27 12:59:17 -0700

    Class work complete..one test not passing...

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -55,14 +55,53 @@ class LinkedList
     end
   end
 
-
+  # Simmilar to the array_lists version of includ? but now we are working with nodes!!
   def include?(key)
+    current = @head # beacuse we don't want to lose the head
+    # traverse through all the noes until the end
+    while current != nil # nil when no items are in the list
+      # check if this noe's data is key
+      if current.data == key
+        # if it is then return true
+        return true
+      end
+      # Need to move 'current' pointer to the next note
+      current = current.next
+      # end Traversing through the list
+    end
+    # return false because I didn't find key
+    return false
   end
 
   def size
+    return @size
+    # current = @head
+    # count = 0
+    # while current != nil
+    #   count += 1
+    #   current = current.next
+    # end
+    # return count
   end
 
   def max
+    # check if list is empty
+    return nil if @size == 0
+    # set max variable
+    biggest = @head.data
+    current = @head
+    # traverse through the linked list until nil
+    while current != nil
+      if current.data > biggest
+        biggest = current.data
+      end
+      current = current.next
+    # check that node pointing to other node is greater than or less than the one its pointing to
+    # save the larger variable
+    # have the nodes check against max variable
+    # save the value that is larges always... should only have to go through linked list once
+    end
+      return biggest
   end
 
   def to_s
@@ -78,3 +117,11 @@ class LinkedList
     return full_list.join("->")
   end
 end
+
+# implementation - how its build in the literal instance, Array and Linked Lists are different
+
+# interface - an abstraction, the similarites between Array and Linked lists are the same
+
+# the point is... you can have shared interfaces that have different implementations
+
+# INTERVIEW QUESTIONS: Linked Lists is a very popular interview type question
